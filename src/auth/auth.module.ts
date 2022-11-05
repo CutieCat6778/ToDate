0fc646bad5267ed48c701be7a5b24bf462fec463
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2022-11-05 21:39:36 +0100

    removed unused import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
 import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { DatabaseModule } from '../database/database.module';
